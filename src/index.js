9740c220800e97f5aa3e417e1621db0db70df777
Fedir <zi-12feodor@ukr.net>
2020-11-03 09:05:15 +0200

    fetch done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 // import './styles.css';
 import './sass/main.scss';
 import './js/render';
+import searchCountryes from './js/fetchCountries';
+const debounceFn = require('lodash.debounce');
+
+const inputEl = document.querySelector('#search-js');
+
+inputEl.addEventListener('input', debounceFn(renderResoult, 500));
+
+function renderResoult(event) {
+  const q = event.target.value;
+  if (q.length < 2) {
+    return;
+  }
+  console.log(q);
+  searchCountryes(q).then(r => {
+    searchResult(r);
+  });
+}
+
+function searchResult(res) {
+  res.map(r => console.log(r));
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+export default function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(r =>
+    r.json(),
+  );
+}
